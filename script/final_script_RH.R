bc154afc16f555353a062299cb1dd0532417050e
rebecca_h2202 <s2091339@ed.ac.uk>
2022-11-18 17:13:01 +0000

    consistent capitalisation in final script

--- script/final_script_RH.R
+++ script/final_script_RH.R
@@ -26,13 +26,13 @@ load("data/LPI_data.Rdata")
 # Filtering out Loggerhead Sea turtle from dataset
 data1 <- filter(data, Species == "caretta")
 
-# determining number of populations for Loggerhead Sea Turtle
+# Determining number of populations for Loggerhead Sea Turtle
 length(unique(data1$id))
 
-# determining number of countries for Loggerhead Sea Turtle
+# Determining number of countries for Loggerhead Sea Turtle
 length(unique(data1$Country.list))
 
-# reshaping data
+# Reshaping data
 long <- gather(data1, "year", "pop", 25:69) %>%                         # Reshaping data into long form
   mutate(year = parse_number(as.character(year))) %>%                   # Extracting numeric values from year column
   mutate(genus_species = paste(Genus, Species, sep=" ")) %>%            # Adding column with genus and species
@@ -43,17 +43,17 @@ long1 <- long %>% na.omit(long)  # Removing NA values
 
 # Cutting down dataframe
 long2 <- long1 %>% group_by(genus_species_id) %>%
-  mutate(maxyear=max(year)) %>%                         # column for most recent year of data collection
-  mutate(minyear=min(year)) %>%                         # column for first year of data collection 
+  mutate(maxyear=max(year)) %>%                         # Column for most recent year of data collection
+  mutate(minyear=min(year)) %>%                         # Column for first year of data collection 
   group_by(id) %>%   
-  mutate(no_observations = length(id)) %>%              # column with number of years with data for each population
-  filter(no_observations>15) %>%                        # only keeping populations with more than 15 years of data
-  filter(!Units == "Total number of female turtles")    # only keeping populations with unit nest counts
+  mutate(no_observations = length(id)) %>%              # Column with number of years with data for each population
+  filter(no_observations>15) %>%                        # Only keeping populations with more than 15 years of data
+  filter(!Units == "Total number of female turtles")    # Only keeping populations with unit nest counts
 
-# determining number of unique population in final dataset
+# Determining number of unique population in final dataset
 length(unique(data1$id))
 
-# determining number of countries in final dataset
+# Determining number of countries in final dataset
 length(unique(data1$Country.list))
 
 # Data distribution ----
@@ -67,8 +67,7 @@ length(unique(data1$Country.list))
    theme(axis.text = element_text(size = 12),
          axis.title = element_text(size = 14, face = "plain")))
 
-# the histogram clearly confirms poisson distribution
-# we have integer values and left-skewed data
+# The histogram clearly confirms poisson distribution. The data is left-skewed.
 
 
 # Statistical analysis ----
@@ -76,14 +75,14 @@ length(unique(data1$Country.list))
 long2$pop <- as.integer(long2$pop)  # making nest counts integers
 
 # Final Model
-# The final model looks at the interaction between country and time
-# Understanding how populations change over time in each country using nest counts as an index
-# the model takes around 20-30 minutes to run so the model output has been saved below
+# The final model looks at the interaction between country and time.
+# Understanding how populations change over time in each country using nest counts as an index.
+# The model takes around 20-30 minutes to run so the model output has been saved below.
 
-# model <- brms::brm(pop ~ I(year - 1973) * Country.list + (1|Location.of.population),   # interaction between country and year, location of pop. random effect
-#                    data = long2, family = poisson(), chains = 3,                       # poisson distribution
+# model <- brms::brm(pop ~ I(year - 1973) * Country.list + (1|Location.of.population),   # Interaction between country and year, location of pop. random effect
+#                    data = long2, family = poisson(), chains = 3,                       # Poisson distribution
 #                    iter = 4000, warmup = 1000,
-#                    control = list(max_treedepth = 15, adapt_delta = 0.9))              # increased maximum tree depth and alpha delta
+#                    control = list(max_treedepth = 15, adapt_delta = 0.9))              # Increased maximum tree depth and alpha delta
 
 # save(model, file = "script/model.RData")
 load("~/data science/challenge3-rebeccah2202/script/model.RData")
@@ -95,17 +94,17 @@ pp_check(model)
 # Figure separate locations: This figure incorporates facet_wrap function to plot countries in separate plots including model predictions
 (location_seperate <- long2 %>%
     group_by(Country.list) %>%
-    add_predicted_draws(model) %>%                                                                  # adding the posterior distribution
-    ggplot(aes(x = year, y = pop, color = ordered(Country.list), fill = ordered(Country.list))) +   # adding colours for different countries
-    stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50), alpha = 1/4) +                 # adding regression line and CI
-    geom_point(data = long2) +                                                                      # adding raw data
-    facet_wrap(~ Country.list, scales = "free_y") +                                                 # separating countries into separate graphs
+    add_predicted_draws(model) %>%                                                                  # Adding the posterior distribution
+    ggplot(aes(x = year, y = pop, color = ordered(Country.list), fill = ordered(Country.list))) +   # Adding colours for different countries
+    stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50), alpha = 1/4) +                 # Adding regression line and CI
+    geom_point(data = long2) +                                                                      # Adding raw data
+    facet_wrap(~ Country.list, scales = "free_y") +                                                 # Separating countries into separate graphs
     theme_bw() +
     ylab("Number of Loggerhead Sea Turtle nests\n") +
     xlab("\nYear") +
-    scale_fill_brewer(palette = "Set2") +                                                           # changing colour palette
+    scale_fill_brewer(palette = "Set2") +                                                           # Changing colour palette
     scale_color_brewer(palette = "Dark2") +
-    theme(legend.title = element_blank(),                                                           # formatting figure
+    theme(legend.title = element_blank(),                                                           # Formatting figure
           legend.position = "none",
           axis.text.x = element_text(size = 12, angle = 45,  vjust = 1, hjust = 1),
           axis.text.y = element_text(size = 12),
@@ -114,12 +113,11 @@ pp_check(model)
           panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
           plot.margin = unit(c(1,1,1,1), units = , "cm"),                 
           panel.spacing = unit(2, "lines"),
-          plot.title = element_text( face = "bold")) +
-    labs(title="Loggerhead Sea Turtle trends between 1973 and 2009 across the world\n"))
+          plot.title = element_text( face = "bold")))
 
-ggsave(filename = 'figures/countries_mod.png', location_seperate, 
+ggsave(filename = 'figures/countries_model.png', location_seperate, 
        device = 'png', width = 10, height = 8)
 
 # Model Output Table ----
-tab_model(model)                      # back-transformed table
-tab_model(model, transform = NULL)    # log scale table
+tab_model(model)                      # Back-transformed table
+tab_model(model, transform = NULL)    # Log scale table
